1cd7ed1dea30a9049a94c4eeb293a16bafe8ba23
Sandra Saluti <sandra.saluti@gmail.com>
2024-01-23 15:03:49 +0100

    added test-module

--- updateUserPhoto.ps1
+++ updateUserPhoto.ps1
@@ -1,4 +1,36 @@
+function test-module {
+    [CmdletBinding()]
+    param(
+        [String]$Name
 
+    )
+    Write-Host "Checking module $name"
+    if (-not (Get-Module $Name)) {
+        Write-Host "Module $Name not imported, trying to import"
+        try {
+            if ($Name -eq 'Microsoft.Graph') {
+                Write-Host "Microsoft.Graph module import takes a while"
+                Import-Module $Name  -ErrorAction Stop
+            }
+            elseif ($Name -eq 'Az') {
+                Write-Host "Module Az is being imported. This might take a while"
+            }
+            else {
+                Import-Module $Name  -ErrorAction Stop
+            }
+
+        }
+        catch {
+            Write-Host "Module $Name not found, trying to install"
+            Install-Module $Name -Scope CurrentUser -AllowClobber -Force -AcceptLicense -SkipPublisherCheck
+            Write-Host "Importing module  $Name "
+            Import-Module $Name  -ErrorAction stop
+        }
+    }
+    else {
+        Write-Host "Module $Name is imported"
+    }
+}
 
 function Update-Photo {
     param (
@@ -9,6 +41,7 @@ function Update-Photo {
 
     )
 
+    Test-module -name Microsoft.Graph.Authentication
     Disconnect-MgGraph
     Connect-MGgraph -scopes user.readwrite.all
 
