5ba523e4f77336bec59704f4367b01ca4e2b4df3
Sandra Saluti <sandra.saluti@crayon.com>
2024-12-12 12:15:20 +0100

    ConsistencyLevel

---
+++ igall.ps1
@@ -0,0 +1,26 @@
+function igall {
+    [CmdletBinding()]
+    param (
+      [string]$Uri,
+      [switch]$Eventual,
+      [int]$limit=1000
+    )
+    $nextUri = $uri
+    $count=0
+    $headers = @{
+      Accept = 'application/json'
+    }
+    if ($Eventual) {
+      $headers.Add('ConsistencyLevel', 'eventual')
+    }
+    do {
+      $result = Invoke-MgGraphRequest -Method GET -uri $nextUri -Headers $headers
+      $nextUri = $result.'@odata.nextLink'
+      if ($result.value) {
+        $result.value | ConvertTo-PSCustomObject
+      } elseif ($result) {
+        $result | ConvertTo-PSCustomObject
+      }
+      $count +=1
+    } while ($nextUri -and ($count -lt $limit))
+  }
