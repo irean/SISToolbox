30703801ccb0a1979f21467fd966a473fdf46440
Sandra Saluti <sandra.saluti@gmail.com>
2025-10-08 12:52:56 +0200

    added convertto-pscustomobject

--- Lenovo Warranty Batch Lookup/lenovo_warranty_batchj_lookup.ps1
+++ Lenovo Warranty Batch Lookup/lenovo_warranty_batchj_lookup.ps1
@@ -37,6 +37,36 @@ function igall {
       $result | ConvertTo-PSCustomObject
     }
   }
+  function ConvertTo-PSCustomObject {
+  [CmdletBinding()]
+  param (
+    [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+    [System.Collections.Hashtable] $InputObject
+  )
+  Process {
+    if ( $InputObject) {
+      $o = New-Object psobject
+      foreach ($key in $InputObject.Keys) {
+        $value = $InputObject[$key]
+        if ($value -and $value.GetType().FullName -match 'System.Object\[\]') {
+          if ($value.Count -gt 0 -and $value[0].GetType().FullName -match  'System.Collections.Hashtable') {
+            $tempVal = $value | ConvertTo-PSCustomObject
+            Add-Member -InputObject $o -NotePropertyName $key -NotePropertyValue $tempVal
+          } elseif ($value.Count -gt 0 -and $value[0].GetType().FullName -match  'System.String') {
+            $tempVal = $value | ForEach-Object { $_ }
+            Add-Member -InputObject $o -NotePropertyName $key -NotePropertyValue $tempVal
+          }
+        } elseif ($value -and $value.GetType().FullName -match 'System.Collections.Hashtable') {
+          Add-Member -InputObject $o -NotePropertyName $key -NotePropertyValue (ConvertTo-PSCustomObject -InputObject $value)
+        } else {
+          Add-Member -InputObject $o -NotePropertyName $key -NotePropertyValue $value
+        }
+      }
+
+      Write-Output $o
+    }
+  }
+}
 
     function test-module {
         [CmdletBinding()]
