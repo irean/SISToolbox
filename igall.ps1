520f4ace580a8adc55bea9384f65a7eda951ea21
Sandra Saluti <sandra.saluti@gmail.com>
2025-10-23 09:17:21 +0200

    update

--- igall.ps1
+++ igall.ps1
@@ -1,26 +1,30 @@
 function igall {
     [CmdletBinding()]
     param (
-      [string]$Uri,
-      [switch]$Eventual,
-      [int]$limit=1000
+        [string]$Uri,
+        [switch]$Eventual,
+        [int]$limit = 1000
     )
     $nextUri = $uri
-    $count=0
+    $count = 0
     $headers = @{
-      Accept = 'application/json'
+        Accept = 'application/json'
     }
     if ($Eventual) {
-      $headers.Add('ConsistencyLevel', 'eventual')
+        $headers.Add('ConsistencyLevel', 'eventual')
     }
     do {
-      $result = Invoke-MgGraphRequest -Method GET -uri $nextUri -Headers $headers
-      $nextUri = $result.'@odata.nextLink'
-      if ($result.value) {
-        $result.value | ConvertTo-PSCustomObject
-      } elseif ($result) {
-        $result | ConvertTo-PSCustomObject
-      }
-      $count +=1
+        $result = Invoke-MgGraphRequest -Method GET -uri $nextUri -Headers $headers
+        $nextUri = $result.'@odata.nextLink'
+        if ($result.value) {
+            $result.value | ConvertTo-PSCustomObject
+        }
+        elseif($result.value.count -eq 0){
+            @()
+        }
+        elseif ($result) {
+            $result | ConvertTo-PSCustomObject
+        }
+        $count += 1
     } while ($nextUri -and ($count -lt $limit))
-  }
+}
